e133e380cba053bad2cd002c3ec077438a518050
azastar-tech <mabouazareel@gmail.com>
2026-01-18 17:31:19 +0100

    vehicle Crud section okay, cuz i've create the models and the database operations

---
+++ app/src/main/java/com/teste/carrent/DataBase/VehicleDao.java
@@ -0,0 +1,43 @@
+package com.teste.carrent.DataBase;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.teste.carrent.Models.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
+public interface VehicleDao {
+
+    @Query("SELECT * FROM Vehicle")
+    List<Vehicle> getAll();
+
+
+    @Query("SELECT * FROM Vehicle WHERE vehicleID = :vehicleID")
+    Vehicle findVehicle(int vehicleID);
+
+
+    @Query("SELECT * FROM Vehicle WHERE category = :category")
+    List<Vehicle> getCategoryVehicle(String category);
+
+    @Query("SELECT * FROM Vehicle WHERE vehicleID = :id")
+    boolean exists(int id);
+
+    @Query("DELETE FROM Vehicle WHERE vehicleID >= 0")
+    void deleteAll();
+
+    @Delete
+    void delete(Vehicle vehicle);
+
+    @Insert
+    void insert(Vehicle vehicle);
+
+    @Update
+    void update(Vehicle vehicle);
+}
+
