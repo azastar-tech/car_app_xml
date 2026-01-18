698f769edd68f01d25f861794db81e7e3adf316d
azastar-tech <mabouazareel@gmail.com>
2026-01-18 17:23:06 +0100

    customer dao

---
+++ app/src/main/java/com/teste/carrent/DataBase/CustomerDao.java
@@ -0,0 +1,45 @@
+package com.teste.carrent.DataBase;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.teste.carrent.Models.Customer;
+
+import java.util.List;
+
+@Dao
+public interface CustomerDao {
+
+    @Query("SELECT * FROM Customer")
+    List<Customer> getAll();
+
+    @Query("SELECT * FROM Customer WHERE email = :email AND password = :password")
+    Customer findUser(String email, String password);
+
+    @Query("SELECT * FROM Customer WHERE customerID = :customerID")
+    Customer findUser(int customerID);
+
+    @Query("SELECT * FROM Customer WHERE email = :email AND firstName = :firstName AND lastName = :lastName")
+    Customer findUser(String firstName, String lastName, String email);
+
+    @Query("DELETE FROM Customer WHERE customerID >= 0")
+    void deleteAll();
+
+    @Query("SELECT * FROM Customer WHERE customerID = :customerID")
+    boolean exist(int customerID);
+
+    @Query( "UPDATE Customer " +
+            "SET gender = :gender WHERE customerID = :customerID")
+    void setTitle(String gender, int customerID);
+
+    @Delete
+    void delete(Customer customer);
+
+    @Insert
+    void insert(Customer customer);
+}
+
+
+
