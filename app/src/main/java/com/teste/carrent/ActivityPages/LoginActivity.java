a9d350c5cef54ea553e9ad1ea1bb0af46c2d8170
azastar-tech <mabouazareel@gmail.com>
2026-01-19 12:31:11 +0100

    login and register  activities okay

--- app/src/main/java/com/teste/carrent/ActivityPages/LoginActivity.java
+++ app/src/main/java/com/teste/carrent/ActivityPages/LoginActivity.java
@@ -3,8 +3,6 @@ package com.teste.carrent.ActivityPages;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,9 +10,7 @@ import android.widget.Toast;
 import androidx.room.Room;
 import com.teste.carrent.DataBase.CustomerDao;
 import com.teste.carrent.DataBase.Project_Database;
-import com.teste.carrent.DataBase.VehicleDao;
 import com.teste.carrent.Models.Customer;
-import com.teste.carrent.Models.Vehicle;
 import com.teste.carrent.Session.Session;
 import com.teste.carrent.R;
 
@@ -26,11 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText password;
 
     private Project_Database db;
-    private Button customer;
-    private Button vehicleCategory;
-    private Button vehicle;
 
-    private Button populate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,83 +51,31 @@ public class LoginActivity extends AppCompatActivity {
 
     private void clickListenHandler(){
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerPage = new Intent(LoginActivity.this, RegistrationActivity.class);
-                startActivity(registerPage);
+        register.setOnClickListener(v -> {
+            Intent registerPage = new Intent(LoginActivity.this, RegistrationActivity.class);
+            startActivity(registerPage);
+        });
+
+        login.setOnClickListener(v -> {
+            CustomerDao customerDao = db.customerDao();
+            Customer check = customerDao.findUser(email.getText().toString(),password.getText().toString());
+
+            if(check != null){
+                Session.save(LoginActivity.this,"customerID",check.getCustomerID()+"");
+                Session.save(LoginActivity.this,"isLoggedIn","true");
+
+                Intent homePage = new Intent(LoginActivity.this,MainActivity.class);
+                homePage.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(homePage);
+            }else{
+                toast();
             }
         });
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CustomerDao customerDao = db.customerDao();
-                Customer check = customerDao.findUser(email.getText().toString(),password.getText().toString());
-
-                if(check != null){
-                    Session.save(LoginActivity.this,"customerID",check.getCustomerID()+"");
-                    Session.save(LoginActivity.this,"isLoggedIn","true");
-
-                    Intent homePage = new Intent(LoginActivity.this,MainActivity.class);
-                    homePage.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(homePage);
-                }else{
-                    toast("Unsuccessful");
-                }
-            }
-        });
-
-        customer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CustomerDao customerDao = db.customerDao();
-                for(Customer c: customerDao.getAll()){
-                    Log.d("MainActivity", "CUSTOMER => " + c.toString());
-                }
-            }
-        });
-
-
-
-        vehicle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                VehicleDao vehicleDao = db.vehicleDao();
-                for(Vehicle c: vehicleDao.getAll()){
-                    Log.d("MainActivity", "VEHICLE => " + c.toString());
-                }
-            }
-        });
-
-
-        populate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                VehicleDao vehicleDao = db.vehicleDao();
-                Vehicle v1 = new Vehicle(273,65.5,5,6497,"nissan","altima",2020,"sedan",true,"https://65e81151f52e248c552b-fe74cd567ea2f1228f846834bd67571e.ssl.cf1.rackcdn.com/ldm-images/2020-Nissan-Altima-Color-Super-Black.png");
-                Vehicle v2 = new Vehicle(285,54.8,5,4578,"toyota","avalon",2020,"sedan",true,"https://img.sm360.ca/ir/w640h390c/images/newcar/ca/2020/toyota/avalon/limited/sedan/main/2020_toyota_avalon_LTD_Main.png");
-                Vehicle v3 = new Vehicle(287,50.99,5,1379,"subaru","wrx",2020,"sedan",true,"https://img.sm360.ca/ir/w640h390c/images/newcar/ca/2020/subaru/wrx/base-wrx/sedan/exteriorColors/12750_cc0640_001_d4s.png");
-
-                Vehicle v4 = new Vehicle(265,58.89,5,6490,"kia","telluride",2020,"suv",true,"https://www.cstatic-images.com/car-pictures/xl/usd00kis061c021003.png");
-                Vehicle v5 = new Vehicle(229,86.5,5,4970,"lincoln","aviator",2020,"suv",true,"https://www.cstatic-images.com/car-pictures/xl/usd00lis021b021003.png");
-                Vehicle v6 = new Vehicle(219,95.0,5,595,"ford","explorer",2020,"suv",true,"https://www.cstatic-images.com/car-pictures/xl/usd00fos102d021003.png");
-
-                Vehicle v7 = new Vehicle(297,56.0,2,200,"chevrolet","camaro",2020,"coupe",false,"https://www.cstatic-images.com/car-pictures/xl/usc90chc022b021003.png");
-
-                vehicleDao.insert(v1);
-                vehicleDao.insert(v2);
-                vehicleDao.insert(v3);
-                vehicleDao.insert(v4);
-                vehicleDao.insert(v5);
-                vehicleDao.insert(v6);
-                vehicleDao.insert(v7);
-            }
-        });
     }
 
-    private void toast(String txt){
-        Toast toast = Toast.makeText(getApplicationContext(),txt,Toast.LENGTH_SHORT);
+    private void toast(){
+        Toast toast = Toast.makeText(getApplicationContext(), "Unsuccessful",Toast.LENGTH_SHORT);
         toast.show();
     }
 }
--- app/src/main/java/com/teste/carrent/ActivityPages/RegistrationActivity.java
+++ app/src/main/java/com/teste/carrent/ActivityPages/RegistrationActivity.java
@@ -1,24 +1,156 @@
 package com.teste.carrent.ActivityPages;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegistrationActivity extends AppCompatActivity {
+import androidx.room.Room;
+
+
+import com.teste.carrent.DataBase.CustomerDao;
+import com.teste.carrent.DataBase.Project_Database;
+import com.teste.carrent.Models.Customer;
+import com.teste.carrent.R;
+
+import java.util.Random;
+
+
+public class RegistrationActivity extends AppCompatActivity{
+    private Button register;
+    private TextView login;
+
+    private TextView expiryDate;
+    private TextView dob;
+    private CustomerDao customerDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        initComponents();
+
+        clickListenHandler();
+    }
+
+    private void initComponents(){
+        register = findViewById(R.id.register);
+
+        login = findViewById(R.id.login);
+
+        expiryDate = findViewById(R.id.expiryDate);
+
+        dob = findViewById(R.id.dob);
+
+        customerDao = Room.databaseBuilder(getApplicationContext(), Project_Database.class, "car_rental_db").allowMainThreadQueries()
+                .build()
+                .customerDao();
+
+    }
+    private void clickListenHandler(){
+
+        expiryDate.setOnClickListener(v -> openCalendar(expiryDate));
+
+        dob.setOnClickListener(v -> openCalendar(dob));
+
+
+        login.setOnClickListener(v -> {
+            Intent registerPage = new Intent(RegistrationActivity.this,LoginActivity.class);
+            startActivity(registerPage);
+        });
+
+
+        register.setOnClickListener(v -> {
+            Customer customer = createCustomerObject();
+
+            if(customerDao != null) {
+
+                if(customer != null) {
+                    customerDao.insert(customer);
+                    toast("Registration Successful");
+                    finish();
+                }
+            }
         });
     }
+
+    private void openCalendar(final TextView dateFieldButton) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this);
+
+        datePickerDialog.setOnDateSetListener((view, year, month, dayOfMonth) -> {
+            String date = year + "-" + month + "-" + dayOfMonth;
+            dateFieldButton.setText(date);
+        });
+
+        datePickerDialog.show();
+    }
+
+
+    private Customer createCustomerObject(){
+
+        String firstName = ((EditText)findViewById(R.id.firstName)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.lastName)).getText().toString();
+
+        String email = ((EditText)findViewById(R.id.email)).getText().toString();
+
+        String driverLicense = ((EditText)findViewById(R.id.license)).getText().toString();
+        String expiry = expiryDate.getText().toString();
+        String dateOfBirth = dob.getText().toString();
+
+        String phoneNumber = ((EditText)findViewById(R.id.phoneNumber)).getText().toString();
+
+        String street = ((EditText)findViewById(R.id.street)).getText().toString();
+        String city = ((EditText)findViewById(R.id.city)).getText().toString();
+        String postalCode = ((EditText)findViewById(R.id.postalCode)).getText().toString();
+
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        String confirm_password = ((EditText)findViewById(R.id.confirmPassword)).getText().toString();
+
+        boolean success = fieldRequiredCheck(firstName,lastName,email,driverLicense,expiry,dateOfBirth,phoneNumber,street,city,postalCode);
+
+        int id = generateID();
+
+        while(customerDao.exist(id)){
+            id = generateID();
+        }
+
+        if(success){
+            if(!password.isEmpty() && password.equals(confirm_password)) {
+                return new Customer(id,firstName, lastName,
+                        email, driverLicense, expiry,
+                        dateOfBirth, phoneNumber, street,
+                        city, postalCode, password
+                );
+            }else{
+                toast("Password do not match");
+            }
+        }else{
+            toast("Incomplete Form");
+        }
+
+        return null;
+    }
+
+    private boolean fieldRequiredCheck(String firstName,String lastName, String email, String driverLicense, String expiry, String dateOfBirth, String phoneNumber, String street, String city, String postalCode) {
+        return  !firstName.isEmpty() && !lastName.isEmpty() &&
+                !email.isEmpty() && !driverLicense.isEmpty() && !expiry.isEmpty() &&
+                !dateOfBirth.isEmpty() && !phoneNumber.isEmpty() && !street.isEmpty() &&
+                !city.isEmpty() && !postalCode.isEmpty();
+    }
+
+    private void toast(String txt){
+        Toast toast = Toast.makeText(getApplicationContext(),txt,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private int generateID(){
+        Random rnd = new Random();
+        return 202000 + rnd.nextInt(65)+10;
+    }
+
 }
